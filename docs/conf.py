6272c3a9c96e863645276e9c676ed28e394d152f
RodrigoATorres <ratorres7@gmail.com>
2021-09-03 20:00:21 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,14 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../hermione/module_templates/__IMPLEMENTED_BASE__/src/'))
+
 project = "Hermione"
 
 extensions = [
     "myst_parser",
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon'
 ]
 # html_theme = "sphinx_rtd_theme"
 source_suffix = [".rst", ".md"]
